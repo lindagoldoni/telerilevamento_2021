5bd369c4afb48442605672cd477d5ade595db016
lindagoldoni <80513382+lindagoldoni@users.noreply.github.com>
2021-03-17 12:42:01 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -11,3 +11,13 @@ p224r63_2011 #posso richiamare la variabile per vedere se me l'ha caricata, si a
 #mi ha caricato un raster brick  (una serie di bande sovrapposte)
 
 plot(p224r63_2011) #mi visualizza le immagini di bande di riflettanza
+
+#vogliamo visualizzare una diversa scala di colore e stabiliamo noi una diversa scala di colori
+#che restituiscono i valori di riflettanza per una determinata lunghezza d'onda
+
+#color change
+color<-colorRampPalette(c("black","grey","light grey"))(100) #dobbiamo racchiuderli in un vettore per dirgli che fanno parte della stessa caratteristica, ovvero il colore, 
+#sono diversi elementi per lo stesso argomento
+#con (100) gli diciamo quanti livelli di colore inserire nella scala
+
+> plot(p224r63_2011, col=color) #con questo comando plottiamo il raster con la nuova colorazione
