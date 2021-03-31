7861fe8aa0ec5f9c8d393ecfc14422241b24a55d
lindagoldoni <80513382+lindagoldoni@users.noreply.github.com>
2021-03-31 13:00:31 +0200

    Create R_code_time_series.r

---
+++ R_code_time_series.r
@@ -0,0 +1,6 @@
+# Time series analysis
+# Greenland increase of temperature
+# Data and code from Emanuela Cosma
+
+library(raster) #richiamiamo il pacchetto raster
+setwd("C:/lab/greenland") #settiamo la cartella di lavoro 
