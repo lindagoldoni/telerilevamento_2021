eeaa439c96eec545a15c1272e10070ef6db9a315
lindagoldoni <80513382+lindagoldoni@users.noreply.github.com>
2021-07-13 15:47:22 +0200

    Update R_code_EN.r

--- R_code_EN.r
+++ R_code_EN.r
@@ -32,4 +32,33 @@ plot(EN01, col=cls, main="NO2 in January")
 plot(EN13, col=cls, main="NO2 in March")
 plot(ENdif, col=cls, main="Difference (January - March)")
 
-#7: 
+#7: importare tutto il set
+
+# list of files:
+rlist <- list.files(pattern="EN")
+rlist
+
+import <- lapply(rlist,raster)
+import
+
+EN <- stack(import)
+plot(EN, col=cls)
+
+# 8: plottare img 1 e 13 dallo stack appena fatto
+
+par(mfrow=c(2,1))
+plot(EN$EN_0001, col=cls)
+plot(EN$EN_0013, col=cls)
+
+# 9: fare una PCA sulle 13 immagini
+
+library(RStoolbox)
+
+ENpca<-rasterPCA(EN)
+summary(EN$model)
+plotRGB(ENpca$map, r=1, g=2, b=3, stretch="lin")
+
+# 10: calcolare la variabilità (deviazione standard) della prima componente pca
+
+PC1sd<-focal(ENpca$map$PC1, w=matrix(1/9,nrow=3,ncol=3),fun=sd)
+plot(PC1sd,col=cls)
