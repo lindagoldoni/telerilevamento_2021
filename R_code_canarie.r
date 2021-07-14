a4e494f5d4e1ef29a3afd7ced60d645e546b5eef
lindagoldoni <80513382+lindagoldoni@users.noreply.github.com>
2021-07-14 18:41:30 +0200

    Update R_code_canarie.r

--- R_code_canarie.r
+++ R_code_canarie.r
@@ -1,6 +1,21 @@
 # CARATTERIZZAZIONE MINERALOGICA/VEGETAZIONALE DELLE ISOLE CANARIE TRAMITE UNSUPERVISORED CLASSIFICATION E NDVI
 
+library(rasterVis)
+library(rasterdiv)
+library(gridExtra)
+library(RStoolbox)
+library(ggplot2)
+library(raster)
+library(ncdf4)
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # TENERIFE
-
-
 setwd("C:/lab/project/landsat/tenerife")
+
+# NDVI
+
+
+
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------
