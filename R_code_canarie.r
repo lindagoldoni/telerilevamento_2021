6e1b5c26d61984491e77ebd358b2a54cac002571
lindagoldoni <80513382+lindagoldoni@users.noreply.github.com>
2021-07-14 18:44:33 +0200

    Update R_code_canarie.r

--- R_code_canarie.r
+++ R_code_canarie.r
@@ -13,6 +13,34 @@ library(ncdf4)
 setwd("C:/lab/project/landsat/tenerife")
 
 # NDVI
+#(NIR-RED)/(NIR+RED)
+
+#NDVI: normalizza il DVI, fa la standardizzazione del DVI sulla somma tra NIR e RED, in modo da ottenere numeri bassi e si possono confrontare immagini con risoluzione radiometrica differente
+#il range dell'NDVI è [-1 , 1]
+
+
+ndvi1<-(defor1$defor1.1-defor1$defor1.2)/(defor1$defor1.1+defor1$defor1.2)
+plot(ndvi1,col=cl) #vediamo che il range della legenda va da -1 a 1
+# si può scrivere anche:
+#ndvi1<-dvi1/(defor1$defor1.1+defor1$defor1.2)
+# in quanto il numeratore era già associato ad una variabile
+# in RStoolbox esistono tantissimi indici da poter calcolare con una funzione "spectralIndices", con un solo comando posso calcolare una moltitudine di indici
+
+ndvi2<-dvi2/(defor2$defor2.1+defor2$defor2.2)
+plot(ndvi2,col=cl) # anche in questo caso viene visualizzata molto bene la differenza di vegetazione
+
+difndvi<-ndvi1-ndvi2
+plot(difndvi,col=cld)
+
+#RStoolbox: spectralIndices
+library(RStoolbox) # dobbiamo richiamare questo pacchetto
+si1<-spectralIndices(defor1,green=3,red=2,nir=1) #dobbiamo dichiarare le bande che abbiamo 
+plot(si1, col=cl)
+# ci sono i DVI e l'NDVI e altri molteplici indici
+#NDWI lavora sull'acqua, quindi non solo sulla vegetazione
+
+si2<-spectralIndices(defor2,green=3,red=2,nir=1) #dobbiamo dichiarare le bande che abbiamo 
+plot(si2, col=cl) #facciamo la stessa cosa per la seconda immagine
 
 
 
